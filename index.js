52ae6db2f05f10d14a25ae5ba46c80445f252d2c
Karang <arthur.hennequin@free.fr>
2020-05-31 16:10:00 +0200

    Modernize vec3, add standard

--- index.js
+++ index.js
@@ -1,167 +1,193 @@
-module.exports = v;
-v.Vec3 = Vec3;
+const re = /\((-?[.\d]+), (-?[.\d]+), (-?[.\d]+)\)/
 
-var re = /\((-?[.\d]+), (-?[.\d]+), (-?[.\d]+)\)/;
+class Vec3 {
+  constructor (x, y, z) {
+    this.x = x
+    this.y = y
+    this.z = z
+  }
 
-function Vec3(x, y, z) {
-  this.x = x;
-  this.y = y;
-  this.z = z;
+  set (x, y, z) {
+    this.x = x
+    this.y = y
+    this.z = z
+    return this
+  };
+
+  update (other) {
+    this.x = other.x
+    this.y = other.y
+    this.z = other.z
+    return this
+  };
+
+  floored () {
+    return new Vec3(Math.floor(this.x), Math.floor(this.y), Math.floor(this.z))
+  };
+
+  floor () {
+    this.x = Math.floor(this.x)
+    this.y = Math.floor(this.y)
+    this.z = Math.floor(this.z)
+    return this
+  };
+
+  offset (dx, dy, dz) {
+    return new Vec3(this.x + dx, this.y + dy, this.z + dz)
+  };
+
+  translate (dx, dy, dz) {
+    this.x += dx
+    this.y += dy
+    this.z += dz
+    return this
+  };
+
+  add (other) {
+    this.x += other.x
+    this.y += other.y
+    this.z += other.z
+    return this
+  };
+
+  subtract (other) {
+    this.x -= other.x
+    this.y -= other.y
+    this.z -= other.z
+    return this
+  };
+
+  plus (other) {
+    return this.offset(other.x, other.y, other.z)
+  };
+
+  minus (other) {
+    return this.offset(-other.x, -other.y, -other.z)
+  };
+
+  scaled (scalar) {
+    return new Vec3(this.x * scalar, this.y * scalar, this.z * scalar)
+  };
+
+  abs () {
+    return new Vec3(Math.abs(this.x), Math.abs(this.y), Math.abs(this.z))
+  };
+
+  volume () {
+    return this.x * this.y * this.z
+  };
+
+  modulus (other) {
+    return new Vec3(
+      euclideanMod(this.x, other.x),
+      euclideanMod(this.y, other.y),
+      euclideanMod(this.z, other.z))
+  };
+
+  distanceTo (other) {
+    var dx = other.x - this.x
+    var dy = other.y - this.y
+    var dz = other.z - this.z
+    return Math.sqrt(dx * dx + dy * dy + dz * dz)
+  };
+
+  equals (other) {
+    return this.x === other.x && this.y === other.y && this.z === other.z
+  };
+
+  toString () {
+    return '(' + this.x + ', ' + this.y + ', ' + this.z + ')'
+  };
+
+  clone () {
+    return this.offset(0, 0, 0)
+  };
+
+  min (other) {
+    return new Vec3(Math.min(this.x, other.x), Math.min(this.y, other.y), Math.min(this.z, other.z))
+  };
+
+  max (other) {
+    return new Vec3(Math.max(this.x, other.x), Math.max(this.y, other.y), Math.max(this.z, other.z))
+  };
+
+  norm () {
+    return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z)
+  };
+
+  unit () {
+    var norm = this.norm()
+    if (norm === 0) {
+      return this.clone()
+    } else {
+      return this.scaled(1 / norm)
+    }
+  };
+
+  scale (scalar) {
+    this.x *= scalar
+    this.y *= scalar
+    this.z *= scalar
+    return this
+  };
+
+  xyDistanceTo (other) {
+    var dx = other.x - this.x
+    var dy = other.y - this.y
+    return Math.sqrt(dx * dx + dy * dy)
+  };
+
+  xzDistanceTo (other) {
+    var dx = other.x - this.x
+    var dz = other.z - this.z
+    return Math.sqrt(dx * dx + dz * dz)
+  };
+
+  yzDistanceTo (other) {
+    var dy = other.y - this.y
+    var dz = other.z - this.z
+    return Math.sqrt(dy * dy + dz * dz)
+  };
+
+  innerProduct (other) {
+    return this.x * other.x + this.y * other.y + this.z * other.z
+  };
+
+  manhattanDistanceTo (other) {
+    return Math.abs(other.x - this.x) + Math.abs(other.y - this.y) + Math.abs(other.z - this.z)
+  };
+
+  toArray () {
+    return [this.x, this.y, this.z]
+  };
 }
 
-function v(x, y, z) {
+function v (x, y, z) {
   if (x == null) {
-    return new Vec3(0, 0, 0);
+    return new Vec3(0, 0, 0)
   } else if (Array.isArray(x)) {
-    return new Vec3(parseFloat(x[0], 10), parseFloat(x[1], 10), parseFloat(x[2], 10));
+    return new Vec3(parseFloat(x[0], 10), parseFloat(x[1], 10), parseFloat(x[2], 10))
   } else if (typeof x === 'object') {
-    return new Vec3(parseFloat(x.x, 10), parseFloat(x.y, 10), parseFloat(x.z, 10));
+    return new Vec3(parseFloat(x.x, 10), parseFloat(x.y, 10), parseFloat(x.z, 10))
   } else if (typeof x === 'string' && y == null) {
-    var match = x.match(re);
+    var match = x.match(re)
     if (match) {
       return new Vec3(
-          parseFloat(match[1], 10),
-          parseFloat(match[2], 10),
-          parseFloat(match[3], 10));
+        parseFloat(match[1], 10),
+        parseFloat(match[2], 10),
+        parseFloat(match[3], 10))
     } else {
-      throw new Error("vec3: cannot parse: " + x);
+      throw new Error('vec3: cannot parse: ' + x)
     }
   } else {
-    return new Vec3(parseFloat(x, 10), parseFloat(y, 10), parseFloat(z, 10));
+    return new Vec3(parseFloat(x, 10), parseFloat(y, 10), parseFloat(z, 10))
   }
 }
 
-Vec3.prototype.set = function(x, y, z) {
-  this.x = x;
-  this.y = y;
-  this.z = z;
-  return this;
-};
-
-Vec3.prototype.update = function(other) {
-  this.x = other.x;
-  this.y = other.y;
-  this.z = other.z;
-  return this;
-};
-
-Vec3.prototype.floored = function() {
-  return new Vec3(Math.floor(this.x), Math.floor(this.y), Math.floor(this.z));
-};
-
-Vec3.prototype.floor = function() {
-  this.x = Math.floor(this.x);
-  this.y = Math.floor(this.y);
-  this.z = Math.floor(this.z);
-  return this;
-};
-
-Vec3.prototype.offset = function(dx, dy, dz) {
-  return new Vec3(this.x + dx, this.y + dy, this.z + dz);
-};
-Vec3.prototype.translate = function(dx, dy, dz) {
-  this.x += dx;
-  this.y += dy;
-  this.z += dz;
-  return this;
-};
-Vec3.prototype.add = function(other) {
-  this.x += other.x;
-  this.y += other.y;
-  this.z += other.z;
-  return this;
-};
-Vec3.prototype.subtract = function(other) {
-  this.x -= other.x;
-  this.y -= other.y;
-  this.z -= other.z;
-  return this;
-};
-Vec3.prototype.plus = function(other) {
-  return this.offset(other.x, other.y, other.z);
-};
-Vec3.prototype.minus = function(other) {
-  return this.offset(-other.x, -other.y, -other.z);
-};
-Vec3.prototype.scaled = function(scalar) {
-  return new Vec3(this.x * scalar, this.y * scalar, this.z * scalar);
-};
-Vec3.prototype.abs = function() {
-  return new Vec3(Math.abs(this.x), Math.abs(this.y), Math.abs(this.z));
-};
-Vec3.prototype.volume = function() {
-  return this.x * this.y * this.z;
-};
-Vec3.prototype.modulus = function(other) {
-  return new Vec3(
-    euclideanMod(this.x, other.x),
-    euclideanMod(this.y, other.y),
-    euclideanMod(this.z, other.z));
-};
-Vec3.prototype.distanceTo = function(other) {
-  var dx = other.x - this.x;
-  var dy = other.y - this.y;
-  var dz = other.z - this.z;
-  return Math.sqrt(dx * dx + dy * dy + dz * dz);
-};
-Vec3.prototype.equals = function(other) {
-  return this.x === other.x && this.y === other.y && this.z === other.z;
-};
-Vec3.prototype.toString = function() {
-  return "(" + this.x + ", " + this.y + ", " + this.z + ")";
-};
-Vec3.prototype.clone = function() {
-  return this.offset(0, 0, 0);
-};
-Vec3.prototype.min = function(other) {
-  return new Vec3(Math.min(this.x, other.x), Math.min(this.y, other.y), Math.min(this.z, other.z));
-};
-Vec3.prototype.max = function(other) {
-  return new Vec3(Math.max(this.x, other.x), Math.max(this.y, other.y), Math.max(this.z, other.z));
-};
-Vec3.prototype.norm = function() {
-  return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
-};
-Vec3.prototype.unit = function() {
-  var norm = this.norm();
-  if(norm === 0) {
-    return this.clone();
-  } else {
-    return this.scaled(1 / norm);
-  }
-};
-Vec3.prototype.scale = function(scalar) {
-  this.x *= scalar;
-  this.y *= scalar;
-  this.z *= scalar;
-  return this;
-};
-Vec3.prototype.xyDistanceTo = function(other) {
-  var dx = other.x - this.x;
-  var dy = other.y - this.y;
-  return Math.sqrt(dx * dx + dy * dy);
-};
-Vec3.prototype.xzDistanceTo = function(other) {
-  var dx = other.x - this.x;
-  var dz = other.z - this.z;
-  return Math.sqrt(dx * dx + dz * dz);
-};
-Vec3.prototype.yzDistanceTo = function(other) {
-  var dy = other.y - this.y;
-  var dz = other.z - this.z;
-  return Math.sqrt(dy * dy + dz * dz);
-};
-Vec3.prototype.innerProduct = function(other) {
-  return this.x * other.x + this.y * other.y + this.z * other.z;
-};
-Vec3.prototype.manhattanDistanceTo = function(other) {
-  return Math.abs(other.x - this.x) + Math.abs(other.y - this.y) + Math.abs(other.z - this.z);
-};
-Vec3.prototype.toArray = function() {
-  return [this.x, this.y, this.z];
-};
-
-function euclideanMod(numerator, denominator) {
-  var result = numerator % denominator;
-  return result < 0 ? result + denominator : result;
+function euclideanMod (numerator, denominator) {
+  var result = numerator % denominator
+  return result < 0 ? result + denominator : result
 }
+
+module.exports = v
+v.Vec3 = Vec3
